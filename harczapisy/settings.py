cc8c774b5f10d4009a4307c29f9c8dc5b73bfeed
Maciej Gilski <gilski.maciej@gmail.com>
2017-11-26 22:48:31 +0100

    added sample app + updated docker compose

--- harczapisy/settings.py
+++ harczapisy/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,9 @@ WSGI_APPLICATION = 'harczapisy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'lednica',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
