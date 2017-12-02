1513353c38bb9a5d37e3c8c3a3fab4f93225f5c4
Maciej Gilski <gilski.maciej@gmail.com>
2017-12-02 12:10:57 +0100

    finished tests for participantlist

--- api/tests.py
+++ api/tests.py
@@ -76,6 +76,7 @@ class CreateParticipantTestCase(TestCase):
         self.participant_data['patrol'] = PatrolList.objects.latest('id').id
 
     def create_participant_test(self, succeed, key=None, value=None):
+        """Create participant test for testing incorrect string values"""
         participant_data = dict(self.participant_data)
         if key is not None:
             participant_data[key] = value
@@ -103,13 +104,14 @@ class CreateParticipantTestCase(TestCase):
         self.create_participant_test(False, 'pesel', '')
 
     def test_creating_participant_wrong_rank(self):
-        """Test if the api has participant creation capability with wrong instructor rank"""
+        """Test if the api has participant creation capability with wrong
+        instructor rank
+        """
         self.create_participant_test(False, 'instructor_rank', 'pwd.')
         self.create_participant_test(False, 'instructor_rank', 'phm.')
         self.create_participant_test(False, 'instructor_rank', 'hm.')
         self.create_participant_test(False, 'instructor_rank', 'pw')
         self.create_participant_test(False, 'instructor_rank', 'ph')
-        self.create_participant_test(False, 'instructor_rank', '')
 
     def test_creating_participant_wrong_email(self):
         """Test if the api has participant creation capability with wrong email"""
@@ -119,3 +121,93 @@ class CreateParticipantTestCase(TestCase):
         self.create_participant_test(False, 'leader_email', '@gmail.com')
         self.create_participant_test(False, 'leader_email', 'leon@')
         self.create_participant_test(False, 'leader_email', '')
+    
+    def create_participant_conditional_field_test(self, succeed, target=None,
+                                                  target_value='', requirement=None,
+                                                  requirement_value=None):
+        """Create participant test for testing conditional fields."""
+        participant_data = dict(self.participant_data)
+        if target is not None:
+            participant_data[target] = target_value
+        if requirement is not None:
+            participant_data[requirement] = requirement_value
+        self.response = self.client.post(
+            reverse('create_participant'),
+            participant_data,
+            format="json")
+        if succeed:
+            code = status.HTTP_201_CREATED
+        else:
+            code = status.HTTP_400_BAD_REQUEST
+        self.assertEqual(self.response.status_code, code)
+
+
+    # EMAIL TESTING
+
+    def test_creating_participant_no_email_leader_false(self):
+        """
+        Test if the api has participant creation capability with false
+        leader and no email
+        """
+        self.create_participant_conditional_field_test(True, target='leader_email',
+                                                       requirement='leader', requirement_value=False)
+
+    def test_creating_participant_no_email_leader_true(self):
+        """
+        Test if the api has participant creation capability with true
+        leader and no email
+        """
+        self.create_participant_conditional_field_test(False, target='leader_email',
+                                                       requirement='leader', requirement_value=True)
+
+    def test_creating_participant_given_email_leader_false(self):
+        """
+        Test if the api has participant creation capability with true
+        leader and no email
+        """
+        self.create_participant_conditional_field_test(False, target=None,
+                                                       requirement='leader', requirement_value=False)
+
+    # last test case covered by test_creating_participant
+
+    # MEDICAL SERVICE TESTING
+
+    # medical service true
+    def test_creating_participant_no_service_number_medical_true(self):
+        """
+        Test if the api has participant creation capability with true
+        medical and no rescue service number
+        """
+        self.create_participant_conditional_field_test(False, target='which_rescue_service',
+                                                       target_value=None,
+                                                       requirement='service_type',
+                                                       requirement_value='med')
+
+    def test_creating_participant_no_course_medical_true(self):
+        """
+        Test if the api has participant creation capability with true
+        medical and no rescue course
+        """
+        self.create_participant_conditional_field_test(False, target='rescue_course',
+                                                       requirement='service_type',
+                                                       requirement_value='med')
+
+    def test_creating_participant_no_certificate_medical_true(self):
+        """
+        Test if the api has participant creation capability with true
+        medical and no rescue course
+        """
+        self.create_participant_conditional_field_test(False, target='rescue_certificate',
+                                                       requirement='service_type',
+                                                       requirement_value='med')
+
+    # medical service false
+    def test_creating_participant_medical_false(self):
+        """
+        Test if the api has participant creation capability with false
+        medical and medical data
+        """
+        self.create_participant_conditional_field_test(False, requirement='service_type',
+                                                       requirement_value='lit')
+
+    
