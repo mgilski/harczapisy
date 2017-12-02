2195ce33a6ba90e3e32605fd013cc1a7e5a1d3e8
Maciej Gilski <gilski.maciej@gmail.com>
2017-12-02 21:39:58 +0100

    added __str__ functions to mailbox plus a few fixes

--- api/models.py
+++ api/models.py
@@ -45,16 +45,24 @@ class ParticipantList(models.Model):
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
-        return f'{first_name} {last_name}'
+        return f'{self.first_name} {self.last_name}'
 
 
-class MailboxList(models.Model);
-    email = EmailField(unique=True)
+class MailboxList(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return str(self.email)
 
 
 class MessageList(models.Model):
+    DIRECTION = (('in', 'in'), ('out', 'out'))
     mailbox = models.ForeignKey(MailboxList, on_delete=models.CASCADE)
-    email = EmailField()
+    email = models.EmailField()
     subject = models.CharField(max_length=255)
     message = models.TextField()
+    direction = models.CharField(max_length=3, choices=DIRECTION)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.email}, {self.direction}: {self.subject}'
