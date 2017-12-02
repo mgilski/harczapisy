0a24418702f7cfbd8605db4dca2e3b970fe452db
Maciej Gilski <gilski.maciej@gmail.com>
2017-12-02 21:43:54 +0100

    added mailbox views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 from .serializers import PatrolListSerializer, ParticipantListSerializer
 from .serializers import PatrolListAdminSerializer, ParticipantListAdminSerializer
-from .models import PatrolList, ParticipantList
+from .serializers import MailboxSerializer, MessageListSerializer
+from .models import PatrolList, ParticipantList, MailboxList, MessageList
+
+
+##### PATROL
 
 class CreatePatrolView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
@@ -13,6 +17,18 @@ class CreatePatrolAdminView(CreatePatrolView):
     serializer_class = PatrolListAdminSerializer
 
 
+class PatrolDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PatrolList.objects.all()
+    serializer_class = PatrolListSerializer
+
+
+class PatrolDetailAdminView(PatrolDetailView):
+    serializer_class = PatrolListAdminSerializer
+
+
+
+##### PARTICIPANT
+
 class CreateParticipantView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = ParticipantList.objects.all()
@@ -23,15 +39,6 @@ class CreateParticipantAdminView(CreateParticipantView):
     serializer_class = ParticipantListAdminSerializer
 
 
-class PatrolDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = PatrolList.objects.all()
-    serializer_class = PatrolListSerializer
-
-
-class PatrolDetailAdminView(PatrolDetailView):
-    serializer_class = PatrolListAdminSerializer
-
-
 class ParticipantDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ParticipantList.objects.all()
     serializer_class = ParticipantListSerializer
@@ -39,3 +46,25 @@ class ParticipantDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 class ParticipantDetailAdminView(ParticipantDetailView):
     serializer_class = ParticipantListAdminSerializer
+
+
+##### MAILBOX
+
+class CreateMailboxView(generics.ListCreateAPIView):
+    queryset = MailboxList.objects.all()
+    serializer_class = MailboxSerializer
+
+
+class DeleteMailboxView(generics.RetrieveDestroyAPIView):
+    queryset = MailboxList.objects.all()
+    serializer_class = MailboxSerializer
+
+
+class CreateMessageView(generics.ListCreateAPIView):
+    queryset = MessageList.objects.all()
+    serializer_class = MessageListSerializer
+
+
+class DeleteMessageView(generics.RetrieveDestroyAPIView):
+    queryset = MessageList.objects.all()
+    serializer_class = MessageListSerializer
