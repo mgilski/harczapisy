b6b8b38a70b437e77c9603916d2d2b5c603c0986
Maciej Gilski <gilski.maciej@gmail.com>
2017-12-03 15:41:42 +0100

    added roken generation

--- api/views.py
+++ api/views.py
@@ -3,6 +3,10 @@ from .serializers import PatrolListSerializer, ParticipantListSerializer
 from .serializers import PatrolListAdminSerializer, ParticipantListAdminSerializer
 from .serializers import MailboxSerializer, MessageListSerializer
 from .models import PatrolList, ParticipantList, MailboxList, MessageList
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
 
 ##### PATROL
@@ -33,6 +37,22 @@ class CreateParticipantView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = ParticipantList.objects.all()
     serializer_class = ParticipantListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+        if serializer.data['leader'] == True:
+            user = User.objects.create_user(username=serializer.data['pesel'],
+                                            email=serializer.data['leader_email'],
+                                            password='wH%^jy`EW)&#AN9Y')
+            token = str(Token.objects.get_or_create(user=user)[0])
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class CreateParticipantAdminView(CreateParticipantView):
