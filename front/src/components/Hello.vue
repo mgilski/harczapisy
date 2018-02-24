d420c28149615edbd32ff9fd0c472bc5320bcbd5
Maciej Gilski <gilski.maciej@gmail.com>
2018-02-24 01:44:11 +0100

    Add working vue-router

--- front/src/components/Hello.vue
+++ front/src/components/Hello.vue
@@ -22,10 +22,10 @@
 
 <script>
 export default {
-  name: 'hello',
+  name: 'Hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App lllllllllll'
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
